c6fb3c5bbeec50f9160d8f901effcdf9de5ef73f
arckoor <33837362+arckoor@users.noreply.github.com>
2022-06-23 01:16:44 +0200

    fix(loop): added missing initialize call

--- battleships/loop/loop.py
+++ battleships/loop/loop.py
@@ -18,6 +18,31 @@ class Loop:  # todo incorporate ability to forfeit
         self.game_loop()
 
     def game_loop(self):
+        self.p1.initialize(self.p2)
+        self.p2.initialize(self.p1)
+        self.placement()
+        self.p1.alive_update()
+        self.p2.alive_update()
+        while 1:
+            self.line()
+            self.p1.shoot(self.p2)  # shoot
+            self.p2.check_sunken()  # check if something sank
+            if self.p2.game.game_over:  # check if game is over
+                print(f"{self.p1.name} won!")
+                self.p1.win()  # show the boards
+                self.p2.win()
+                break
+            self.line()
+            self.p2.shoot(self.p1)  # same as above
+            self.p1.check_sunken()
+            if self.p1.game.game_over:
+                print(f"{self.p2.name} won!")
+                self.p2.render()
+                self.p1.win()
+                self.p2.win()
+                break
+
+    def placement(self):
         if self.random_board:  # place ships randomly
             self.p1.game.random_placement(self.p1.ships)
             self.p2.game.random_placement(self.p2.ships)
@@ -26,26 +51,6 @@ class Loop:  # todo incorporate ability to forfeit
             self.spacer()  # spacer so players don't reveal their board to each other
             self.p2.placement()
             self.spacer()
-        self.p1.alive_update()
-        self.p2.alive_update()
-        while 1:
-            self.line()
-            self.p1.shoot(self.p2)  # shoot
-            self.p2.check_sunken()  # check if something sank
-            if self.p2.game.get_game_over():  # check if game is over
-                print(f"{self.p1.name} won!")
-                self.p1.win()  # show the boards
-                self.p2.win()
-                break
-            self.line()
-            self.p2.shoot(self.p1)  # same as above
-            self.p1.check_sunken()
-            if self.p1.game.get_game_over():
-                print(f"{self.p2.name} won!")
-                self.p2.render()
-                self.p1.win()
-                self.p2.win()
-                break
 
     def spacer(self):
         print(self.red_line)
